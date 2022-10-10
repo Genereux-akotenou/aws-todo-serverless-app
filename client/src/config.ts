26edbe1fc7e265d089b7a3445fd8d01b8ee18eea
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-10-10 11:53:53 +0100

    add client code

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'q7sjjtlid8'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-9pw3oyy6.us.auth0.com',
+  clientId: 'xgblWqN8KIB6WnOGh6xbeMLHdgoEP4ly',
+  callbackUrl: 'http://localhost:3000/callback'
+}
